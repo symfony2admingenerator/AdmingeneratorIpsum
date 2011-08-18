2669cd4b6d2fa8caba35e1a209229d95dc370d66
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-18 22:05:11 +0200

    Add rebuild shell

---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Rebuild doctrine fixtures
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+php app/console doctrine:schema:create
+php app/console doctrine:fixtures:load
+
