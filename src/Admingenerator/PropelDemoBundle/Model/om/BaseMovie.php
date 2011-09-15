445ad5955c0c720784510f9840d3b62c98af1dba
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-09-15 23:28:01 +0200

    Fix propel dates

--- src/Admingenerator/PropelDemoBundle/Model/om/BaseMovie.php
+++ src/Admingenerator/PropelDemoBundle/Model/om/BaseMovie.php
@@ -142,7 +142,7 @@ abstract class BaseMovie extends BaseObject  implements Persistent
 	 * @return     mixed Formatted date/time value as string or DateTime object (if format is NULL), NULL if column is NULL, and 0 if column value is 0000-00-00
 	 * @throws     PropelException - if unable to parse/validate the date/time value.
 	 */
-	public function getReleaseDate($format = '%x')
+	public function getReleaseDate($format = NULL)
 	{
 		if ($this->release_date === null) {
 			return null;
