9d20929d27ab1235febd46b551ed363ba597cbd6
clombardot <cedric.lombardot@gmail.com>
2012-06-11 11:52:37 +0200

    Update deps

--- src/Admingenerator/DemoBundle/Menu/AdminMenu.php
+++ src/Admingenerator/DemoBundle/Menu/AdminMenu.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 
 namespace Admingenerator\DemoBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
-class AdminMenu
+class AdminMenu extends ContainerAware
 {
     private $factory;
 
@@ -16,7 +17,7 @@ class AdminMenu
     {
         $this->factory = $factory;
     }
-    
+
     /**
      * @param Request $request
      * @param Router $router
@@ -26,31 +27,31 @@ class AdminMenu
         $menu = $this->factory->createItem('root');
 
         $menu->setCurrentUri($request->getRequestUri());
-        
+
         $menu->setchildrenAttributes(array('id' => 'main_navigation', 'class'=>'menu'));
 
         //Propel demos
         $propel = $menu->addChild('Propel', array('uri' => '#'));
         $propel->setLinkAttributes(array('class'=>'sub main'));
         $propel->addChild('Movies', array('route' => 'Admingenerator_PropelDemoBundle_list'));
-        
+
         //Doctrine ORM demos
         $doctrine = $menu->addChild('Doctrine ORM', array('uri' => '#'));
         $doctrine->setLinkAttributes(array('class'=>'sub main'));
         $doctrine->addChild('Movies', array('route' => 'Admingenerator_DemoBundle_list'));
-        
+
         //Doctrine ODM demos
         $doctrine = $menu->addChild('Doctrine ODM', array('uri' => '#'));
         $doctrine->setLinkAttributes(array('class'=>'sub main'));
         $doctrine->addChild('Movies', array('route' => 'Admingenerator_DoctrineODMDemoBundle_list'));
         $doctrine->addChild('Actors', array('route' => 'Admingenerator_DoctrineODMDemoBundle_Actor_list'));
-        
+
         //Help
         $help = $menu->addChild('Overwrite this menu', array('uri' => '#'));
         $help->setLinkAttributes(array('class'=>'sub main'));
         $help->addChild('Configure menu class', array('uri' => 'https://github.com/knplabs/KnpMenuBundle/blob/master/Resources/doc/index.md'));
         $help->addChild('Configure php class to use', array('uri' => 'https://github.com/cedriclombardot/AdmingeneratorGeneratorBundle/blob/master/Resources/doc/change-the-menu-class.markdown'));
-        
+
         return $menu;
     }
 }
--- src/Admingenerator/PropelDemoBundle/Form/Type/ActorType.php
+++ src/Admingenerator/PropelDemoBundle/Form/Type/ActorType.php
@@ -3,21 +3,21 @@
 namespace Admingenerator\PropelDemoBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class ActorType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name');
     }
-    
+
     public function getName()
     {
         return 'propel_embed_actor';
     }
-    
-    public function getDefaultOptions(array $options)
+
+    public function getDefaultOptions()
     {
         return array(
             'data_class' => 'Admingenerator\PropelDemoBundle\Model\Actor',
